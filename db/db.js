325c731c8d184efbbda70b8b71361cffef7eb8da
Mauricio Garcia-Paez <M_garciapae@uncg.edu>
2025-04-15 20:00:03 -0400

    backend is complete, next step is front end

--- db/db.js
+++ db/db.js
@@ -1,12 +1,18 @@
 const sqlite3 = require('sqlite3').verbose();
 const { open } = require('sqlite');
 
+const path = require('path');
+
 async function connectToDatabase() {
+  const dbFilePath = path.join(__dirname, 'jokebook.db');
+  console.log('Using DB file at:', dbFilePath);
+
   const db = await open({
-    filename: './jokebook.db',
+    filename: dbFilePath,
     driver: sqlite3.Database,
   });
   return db;
 }
+
 
 module.exports = connectToDatabase;
